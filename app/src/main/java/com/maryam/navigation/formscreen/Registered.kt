611aa288d0c59d1748febd6de67bd18db72ab0da
mary8 <mary8shtr@gmail.com>
2020-08-01 05:50:18 +0430

    first commit

---
+++ app/src/main/java/com/maryam/navigation/formscreen/Registered.kt
@@ -0,0 +1,22 @@
+package com.maryam.navigation.formscreen
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.maryam.navigation.R
+
+
+/**
+ * Shows "Done".
+ */
+class Registered : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_registered, container, false)
+    }
+}
