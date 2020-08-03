4b10e72b483d069bfb11ec98034416a67f7f0e48
mary8 <mary8shtr@gmail.com>
2020-08-03 10:14:15 +0430

    add mitch solution to this branch

--- app/src/main/java/com/maryam/navigation/MainActivity.kt
+++ app/src/main/java/com/maryam/navigation/MainActivity.kt
@@ -2,59 +2,103 @@ package com.maryam.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.maryam.navigation.formscreen.Register
+import com.maryam.navigation.homescreen.About
+import com.maryam.navigation.listscreen.UserProfile
 
 /**
  * An activity that inflates a layout that has a [BottomNavigationView].
  */
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,BottomNavController.NavGraphProvider,
+    BottomNavController.OnNavigationGraphChanged,
+    BottomNavController.OnNavigationReselectedListener {
 
-    private var currentNavController: LiveData<NavController>? = null
+    private val bottomNavController by lazy(LazyThreadSafetyMode.NONE) {
+        BottomNavController(
+            this,
+            R.id.main_fragments_container,
+            R.id.home,
+            this,
+        this)
+    }
 
+    override fun onGraphChange() {
+    }
+
+    override fun onReselectNavItem(
+        navController: NavController,
+        fragment: Fragment
+    ){
+        when(fragment){
+
+            is UserProfile -> {
+                navController.navigate(R.id.action_userProfile_to_leaderboard)
+            }
+
+            is About -> {
+                navController.navigate(R.id.action_aboutScreen_to_titleScreen)
+            }
+
+            is Register -> {
+                navController.navigate(R.id.action_registered_to_register)
+            }
+            else -> {
+                // do nothing
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomNavigationView(savedInstanceState)
+    }
+
+    private fun setupBottomNavigationView(savedInstanceState: Bundle?){
+       val  bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavigationView.setUpNavigation(bottomNavController, this)
         if (savedInstanceState == null) {
-            setupBottomNavigationBar()
-        } // Else, need to wait for onRestoreInstanceState
+            bottomNavController.setupBottomNavigationBackStack(null)
+            bottomNavController.onNavigationItemSelected()
+        }
+        else{
+            (savedInstanceState[BOTTOM_NAV_BACKSTACK_KEY] as IntArray?)?.let { items ->
+                val backstack = BottomNavController.BackStack()
+                backstack.addAll(items.toTypedArray())
+                bottomNavController.setupBottomNavigationBackStack(backstack)
+            }
+        }
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        super.onRestoreInstanceState(savedInstanceState)
-        // Now that BottomNavigationBar has restored its instance state
-        // and its selectedItemId, we can proceed with setting up the
-        // BottomNavigationBar with Navigation
-        setupBottomNavigationBar()
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // save backstack for bottom nav
+        outState.putIntArray(BOTTOM_NAV_BACKSTACK_KEY, bottomNavController.navigationBackStack.toIntArray())
     }
 
-    /**
-     * Called on first creation and when restoring state.
-     */
-    private fun setupBottomNavigationBar() {
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+    override fun onBackPressed() = bottomNavController.onBackPressed()
 
-        val navGraphIds = listOf(R.navigation.home, R.navigation.list, R.navigation.form)
 
-        // Setup the bottom navigation view with a list of navigation graphs
-        val controller = bottomNavigationView.setupWithNavController(
-            navGraphIds = navGraphIds,
-            fragmentManager = supportFragmentManager,
-            containerId = R.id.nav_host_container,
-            intent = intent
-        )
-
-        // Whenever the selected controller changes, setup the action bar.
-        controller.observe(this, Observer { navController ->
-            setupActionBarWithNavController(navController)
-        })
-        currentNavController = controller
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return currentNavController?.value?.navigateUp() ?: false
+    override fun getNavGraphId(itemId: Int): Int  = when(itemId){
+        R.id.home -> {
+            R.navigation.home
+        }
+        R.id.form -> {
+            R.navigation.form
+        }
+        R.id.list -> {
+            R.navigation.list
+        }
+        else -> {
+            R.navigation.home
+        }
     }
 }
